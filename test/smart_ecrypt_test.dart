fabf436bfb406d696f1dfd3a8f701e55ebb4f5a2
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-16 17:57:57 +0300

    rmv unused tests

--- test/smart_ecrypt_test.dart
+++ test/smart_ecrypt_test.dart
@@ -1,13 +1,12 @@
-import 'package:smart_ecrypt/smart_ecrypt.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = SmartEncrypt();
+    // final awesome = SmartEncrypt();
 
-    setUp(() {
-      // Additional setup goes here.
-    });
+    // setUp(() {
+    //   // Additional setup goes here.
+    // });
 
     // test('First Test', () {
     //   expect(awesome.isAwesome, isTrue);
