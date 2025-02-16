8e4c66ceb219701f8a23792e4fae6625cb19b00a
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-16 17:58:58 +0300

    add getRandomNumbers to smart encrypt

--- lib/src/smart_ecrypt_base.dart
+++ lib/src/smart_ecrypt_base.dart
@@ -1,5 +1,11 @@
-// TODO: Put public facing types in this file.
+
+import 'dart:typed_data';
+
+import 'package:smart_ecrypt/src/crypto_random.dart';
 
 /// Checks if you are awesome. Spoiler: you are.
 class SmartEncrypt {
+  static Uint8List getRandomNumbers(int length){
+    return CryptoRandom.generateBytes(length);
+  }
 }
