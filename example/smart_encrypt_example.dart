c5cb605a5ad023ef380200bdd007f6a05a5fd694
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-16 20:01:58 +0300

    ref

--- example/smart_encrypt_example.dart
+++ example/smart_encrypt_example.dart
@@ -6,12 +6,12 @@ void main() {
   print(SmartEncrypt.getRandomNumbers(24));
   print(SmartEncrypt.getRandomString(30));
   print(SmartEncrypt.createDataHash256('Hello world'));
-  
+
   String str = 'Hello world';
   Uint8List key = SmartEncrypt.createKey();
   Uint8List iv = SmartEncrypt.createIV();
 
- str = SmartEncrypt.encrypt(str, key, iv);
- print(str);
- print(SmartEncrypt.decrypt(str, key, iv));
+  str = SmartEncrypt.encrypt(str, key, iv);
+  print(str);
+  print(SmartEncrypt.decrypt(str, key, iv));
 }
