44f3b5d78c3ddce2225ce2074d16faf6f482ebbb
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-16 18:26:42 +0300

    ref

--- lib/src/smart_ecrypt_base.dart
+++ lib/src/smart_ecrypt_base.dart
@@ -1,15 +1,14 @@
-
 import 'dart:typed_data';
 
 import 'package:smart_ecrypt/src/crypto_random.dart';
 
 /// Checks if you are awesome. Spoiler: you are.
 class SmartEncrypt {
-  static Uint8List getRandomNumbers(int length){
+  static Uint8List getRandomNumbers(int length) {
     return CryptoRandom.generateBytes(length);
   }
 
-  static String getRandomString(int length){
+  static String getRandomString(int length) {
     return CryptoRandom.generateString(length);
   }
 }
