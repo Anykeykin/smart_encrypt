0cb71f71f0b5880d3871420a8a09aa1a0514ac42
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-16 21:51:18 +0300

    upd example

--- example/smart_encrypt_example.dart
+++ example/smart_encrypt_example.dart
@@ -1,8 +1,9 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:smart_encrypt/smart_encrypt.dart';
 
-void main() {
+void main() async {
   print(SmartEncrypt.getRandomNumbers(24));
   print(SmartEncrypt.getRandomString(30));
   print(SmartEncrypt.createDataHash256('Hello world'));
@@ -14,4 +15,11 @@ void main() {
   str = SmartEncrypt.encrypt(str, key, iv);
   print(str);
   print(SmartEncrypt.decrypt(str, key, iv));
+  List<int> yourFileBites = await File('your file path').readAsBytes();
+  print('encrypt start');
+  List<int> encryptedFileBites =
+      await SmartEncrypt.fastEncryptFile(yourFileBites, 250);
+  print('encrypt end');
+  await SmartEncrypt.fastDecryptFile(encryptedFileBites, 250)
+      .then((value) => print('decrypted'));
 }
