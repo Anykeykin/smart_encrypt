54b1470dcf0649fbeb3a7065c09bcc639855106b
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-16 19:54:39 +0300

    add decrypt & encrypt features on example

--- example/smart_encrypt_example.dart
+++ example/smart_encrypt_example.dart
@@ -1,7 +1,17 @@
+import 'dart:typed_data';
+
 import 'package:smart_encrypt/smart_encrypt.dart';
 
 void main() {
   print(SmartEncrypt.getRandomNumbers(24));
   print(SmartEncrypt.getRandomString(30));
   print(SmartEncrypt.createDataHash256('Hello world'));
+  
+  String str = 'Hello world';
+  Uint8List key = SmartEncrypt.createKey();
+  Uint8List iv = SmartEncrypt.createIV();
+
+ str = SmartEncrypt.encrypt(str, key, iv);
+ print(str);
+ print(SmartEncrypt.decrypt(str, key, iv));
 }
