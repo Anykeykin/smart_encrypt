694a3eba3caace6be5a98dceb08251a99dbc0a37
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-16 17:49:57 +0300

    Create smart_ecrypt_test.dart

---
+++ test/smart_ecrypt_test.dart
@@ -0,0 +1,16 @@
+import 'package:smart_ecrypt/smart_ecrypt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
