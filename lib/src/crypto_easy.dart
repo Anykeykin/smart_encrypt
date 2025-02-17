fe34e2ee1790acb915399ca294c10d84b150574b
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-18 00:48:49 +0300

    add crypto easy methods realization

--- lib/src/crypto_easy.dart
+++ lib/src/crypto_easy.dart
@@ -1,8 +1,18 @@
+import 'package:encrypt/encrypt.dart';
+
 class CryptoLight{
-  static String decrypt(String data){
-    return '';
+  static final _key = Key.fromUtf8('32-characters-long-secret-key!');
+  static final _iv = IV.fromLength(16);
+
+  static String encrypt(String input) {
+    final encrypter = Encrypter(AES(_key));
+    final encrypted = encrypter.encrypt(input, iv: _iv);
+    return encrypted.base64;
   }
-  static String encrypt(String data){
-    return '';
+
+  static String decrypt(String input) {
+    final encrypter = Encrypter(AES(_key));
+    final decrypted = encrypter.decrypt64(input, iv: _iv);
+    return decrypted;
   }
 }
