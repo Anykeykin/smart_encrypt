f793a4c3636cd36e08ad7744e40804e9c5be363d
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-22 23:17:41 +0300

    add extract file from image

--- lib/src/crypto_steganography_files.dart
+++ lib/src/crypto_steganography_files.dart
@@ -49,4 +49,44 @@ class Steganography {
     final outputImageFile = File(outputImagePath);
     await outputImageFile.writeAsBytes(encodePng(image));
   }
+
+  static Future<void> extractFileFromImage({
+    required String imagePath,
+    required String outputFilePath,
+    required int fileSize,
+  }) async {
+    final imageFile = File(imagePath);
+    final image = decodeImage(await imageFile.readAsBytes())!;
+
+    final fileBytes = <int>[];
+    int byte = 0;
+    int bitIndex = 0;
+
+    outerLoop:
+    for (var y = 0; y < image.height; y++) {
+      for (var x = 0; x < image.width; x++) {
+        final pixel = image.getPixel(x, y);
+
+        final r = getRed(pixel);
+
+        if (fileBytes.length < fileSize) {
+          final bit = r & 1;
+
+          byte = (byte << 1) | bit;
+          bitIndex++;
+
+          if (bitIndex == 8) {
+            fileBytes.add(byte);
+            byte = 0;
+            bitIndex = 0;
+          }
+        } else {
+          break outerLoop;
+        }
+      }
+    }
+
+    final outputFile = File(outputFilePath);
+    await outputFile.writeAsBytes(fileBytes);
+  }
 }
